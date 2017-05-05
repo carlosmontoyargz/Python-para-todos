55d572cdb6b870c2d3309882708db49a6402af7b
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-05-05 02:08:39 -0500

    Initial commit

---
+++ 01_TiposBasicos/Numeros.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Type(entero1) daria int
+entero1 = 23
+
+# Type(entero2) daria long
+entero2 = 23L
+
+# 027 octal = 23 en base 10
+entero3 = 027
+
+# 0x17 hexadecimal, anteponiendo un 0x
+entero4 = 0x17
+
+print entero1
+print entero2
+print entero3
+print entero4
---
+++ 01_TiposBasicos/TiposBasicos.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Esto es una cadena
+c = "Hola mundo!"
+
+# Y esto es un entero
+e = 23
+
+# Podemos comprobarlo con la funcion type
+print type(c);
+print type(e);
